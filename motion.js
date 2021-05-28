073086d5d4603a0a3baef1b9fb34834481ebac2b
Reilly Kinz <>
2021-05-28 13:12:21 +0000

    assignment complete

--- motion.js
+++ motion.js
@@ -12,3 +12,17 @@ function myMove() {
 		  clearInterval(id);
   }
 }
+function motion() {
+  var elem = document.getElementById("move");   
+  var posTop = 0;
+  var posright = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+	  posTop++;
+	  posright++;
+	  elem.style.top = posTop + "px"; 
+	  elem.style.right = posright + "px"; 
+	  if ((posTop == 350) && (posright == 350))
+		  clearInterval(id);
+  }
+}
